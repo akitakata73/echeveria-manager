658b969aac78ee525bf9581e1a50017924ea4570
akitakata73 <mail@masudakazuma.com>
2026-04-21 23:34:54 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="echeveria-pwa-v42-check-label-ui";
+const CACHE_NAME="echeveria-pwa-v43-overdue-only-photo-cleanup";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
